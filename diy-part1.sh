012ff2c0c8876a77e1b32c8dc16e06df18f50b47
Zane <69372129+IMZane@users.noreply.github.com>
2021-02-01 21:32:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # 添加ADGuard Home软件包
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
+./scripts/feeds update -a
+./scripts/feeds install -a
